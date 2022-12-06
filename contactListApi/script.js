5bf87d31cae28c9d3c9654791505adfe3545c5ca
Natali <nevro1308@gmail.com>
2022-12-06 09:25:40 +0200

    changed file

--- contactListApi/script.js
+++ contactListApi/script.js
@@ -35,7 +35,7 @@ function getContactList() {
     contactListApi
         .getList()
         .then((contactList) => {
-            contactList.forEach((contact) => renderContact(contact));
+            contactList.forEach((contact) => renderContactList(contact));
         })
         .catch(showError);
 }
@@ -43,7 +43,7 @@ function getContactList() {
 function createContact(contact) {
     contactListApi
         .create(contact)
-        .then((contact) => renderContact(contact))
+        .then((contact) => renderContactList(contact))
         .catch(showError);
 }
 
@@ -69,7 +69,7 @@ function addContact() {
     createContact(contact);
 }
 
-function renderContact(contact) {
+function renderContactList(contact) {
     const html = generateContactHTML(contact);
 
     existedContacts.insertAdjacentHTML("beforeend", html);
